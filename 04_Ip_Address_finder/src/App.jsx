5ae191b902fbf26e82a96f9ff3367994a4fba044
monsafali <monsafali272@gmail.com>
2025-01-11 14:17:41 +0500

    complete project

--- 04_Ip_Address_finder/src/App.jsx
+++ 04_Ip_Address_finder/src/App.jsx
@@ -1,26 +1,30 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 // import icon from "./Icon";
 
 import arrow from "./images/icon-arrow.svg";
 import background from "./images/pattern-bg-desktop.png";
 import React, { useEffect, useState } from "react";
+import Markerposition from "./Markerposition";
 
 function App() {
   const [address, setAddress] = useState(null);
-  const [ipAddress, setIpAddress] = "";
+  const [ipAddress, setIpAddress] = useState("");
+  const checkIpAddress =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+  const checkDomain =
+    /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
 
   useEffect(() => {
     try {
       const getInitialData = async () => {
         const apiKey = import.meta.env.VITE_API_KEY;
         const res = await fetch(
-          `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=8.8.8.8`
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=192.212.174.101`
         );
         const data = await res.json();
         setAddress(data);
-        console.log(data);
       };
       getInitialData();
     } catch (error) {
@@ -28,6 +32,27 @@ function App() {
     }
   }, []);
 
+  async function getEnteredAddress() {
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const res = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&${
+        checkIpAddress.test(ipAddress)
+          ? `ipAddress=${ipAddress}`
+          : checkDomain.test(ipAddress)
+          ? `domain=${ipAddress}`
+          : ""
+      }`
+    );
+    const data = await res.json();
+    setAddress(data);
+    console.log(data);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    getEnteredAddress();
+    setIpAddress("");
+  }
   return (
     <>
       <section>
@@ -39,7 +64,11 @@ function App() {
             IP Address Tracker
           </h1>
 
-          <form action="" className="flex justify-center max-w-xl mx-auto">
+          <form
+            onSubmit={handleSubmit}
+            autoCapitalize="off"
+            className="flex justify-center max-w-xl mx-auto"
+          >
             <input
               type="text"
               name="ipaddress"
@@ -47,6 +76,8 @@ function App() {
               placeholder="Search for any ip address domain"
               required
               className="py-2 px-4 rounded-l-lg w-full"
+              value={ipAddress}
+              onChange={(e) => setIpAddress(e.target.value)}
             />
             <button
               type="submit"
@@ -109,11 +140,7 @@ function App() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={[address.location.lat, address.location.lng]}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
+              <Markerposition address={address} />
             </MapContainer>
           </>
         )}
